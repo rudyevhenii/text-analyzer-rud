d3010af41bfb80570ddf0f7f858b5aac5c8957c9
yevhenii.r <yevhenii.r@innovatel.tech>
2026-04-05 16:26:31 +0300

    Add TextStatsController for text statistics API

---
+++ src/main/java/lab/devops/text_analyzer_rud/controller/TextStatsController.java
@@ -0,0 +1,28 @@
+package lab.devops.text_analyzer_rud.controller;
+
+import lab.devops.text_analyzer_rud.model.TextReq;
+import lab.devops.text_analyzer_rud.model.TextStatsRes;
+import lab.devops.text_analyzer_rud.service.TextStats;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/text-stats")
+@RequiredArgsConstructor
+public class TextStatsController {
+
+    private final TextStats textStats;
+
+    @PostMapping
+    public ResponseEntity<TextStatsRes> createTextStats(@RequestBody TextReq request) {
+        TextStatsRes response = textStats.createTextStats(request);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
+}
