cd313d7e52f7dc768b2b5d7c7ef07fb7ff3f39d3
yevhenii.r <yevhenii.r@innovatel.tech>
2026-04-05 10:30:36 +0300

    init project structure

---
+++ src/main/java/lab/devops/text_analyzer_rud/TextAnalyzerRudApplication.java
@@ -0,0 +1,13 @@
+package lab.devops.text_analyzer_rud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerRudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyzerRudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/devops/text_analyzer_rud/TextAnalyzerRudApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.devops.text_analyzer_rud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerRudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
